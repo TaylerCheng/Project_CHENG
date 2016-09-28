34bab6b4fc89af62d1eaf426443104fbb31df2c4
Tayler Cheng <guangchengcc@163.com>
2016-09-28 14:49:27 +0800

    Use spring to test

--- WebStudy/src/test/java/com/cg/web/cxf/client/HelloWorldClient.java
+++ WebStudy/src/test/java/com/cg/web/cxf/client/HelloWorldClient.java
@@ -3,22 +3,53 @@ package com.cg.web.cxf.client;
 import com.cg.web.cxf.service.HelloWorld;
 import com.cg.web.pojo.User;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Cheng Guang on 2016/9/6.
  */
-public class HelloWorldClient
-{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/spring-cxf-consumer.xml" })
+public class HelloWorldClient {
 
-	public static void main( String[] args )
-	{
-		JaxWsProxyFactoryBean svr = new JaxWsProxyFactoryBean( );
-		svr.setServiceClass( HelloWorld.class );
-		svr.setAddress( "http://localhost:8080/helloWorld" );
-		HelloWorld hw = (HelloWorld) svr.create( );
-		User user = new User( );
-		user.setName( "Cheng" );
-		user.setSex( "test" );
-		System.out.println( hw.sayHiToUser( user ) );
-	}
+    @Autowired
+    private HelloWorld client;
+
+    @Test
+    public void testWithoutSpring() {
+        JaxWsProxyFactoryBean svr = new JaxWsProxyFactoryBean();
+        svr.setServiceClass(HelloWorld.class);
+        svr.setAddress("http://localhost:8080/webstudy/webservice/helloWorld");
+        HelloWorld hw = (HelloWorld) svr.create();
+        User user = new User();
+        user.setName("Cheng");
+        user.setSex("test");
+        System.out.println(hw.sayHiToUser(user));
+    }
+
+    @Test
+    public void testBySpring() {
+        User user1 = new User();
+        user1.setName("Cheng");
+        user1.setSex("男");
+
+        User user2 = new User();
+        user2.setName("freeman");
+        user1.setSex("女");
+
+        List<User> userList = new ArrayList<User>();
+        userList.add(user1);
+        userList.add(user2);
+
+        String[] res = client.SayHiToUserList(userList);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
+    }
 }
