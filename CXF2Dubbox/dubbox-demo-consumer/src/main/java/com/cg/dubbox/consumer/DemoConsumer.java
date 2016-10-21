bc5c313a7e1d3b72ceea83ef7e46206ed7de9b51
Tayler Cheng <guangchengcc@163.com>
2016-10-21 14:02:03 +0800

    dubbox使用注解

--- CXF2Dubbox/dubbox-demo-consumer/src/main/java/com/cg/dubbox/consumer/DemoConsumer.java
+++ CXF2Dubbox/dubbox-demo-consumer/src/main/java/com/cg/dubbox/consumer/DemoConsumer.java
@@ -4,6 +4,7 @@ package com.cg.dubbox.consumer;
  * Created by Cheng Guang on 2016/9/29.
  */
 
+import com.cg.dubbox.api.service.DataRestService;
 import com.cg.dubbox.api.service.MyService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,16 +17,19 @@ public class DemoConsumer {
 
     public static void main(String[] args) {
         //测试常规服务
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath*:spring-dubbo-consumer.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "classpath*:spring-dubbo-consumer.xml");
         context.start();
         //使用dubbox框架，可以像调用本地服务一样调用远程程序
         MyService userService = context.getBean(MyService.class);
-        System.out.println("结果-----------------: "+ userService.getUserInXML().getUsername());
+        System.out.println("结果-----------------: " + userService.getUserInXML().getUsername());
 
+        DataRestService infoCollectDataRestService = context.getBean(DataRestService.class);
+        System.out.println("结果-----------------: " + infoCollectDataRestService.getCitiesData());
 
         //测试Restful服务
-//        String url = "http://localhost:8888/services/json/hello";
-//        System.out.println("Successfully got result: " + getUser(url));
+        //        String url = "http://localhost:8888/services/json/hello";
+        //        System.out.println("Successfully got result: " + getUser(url));
     }
 
     private static String getUser(String url) {
