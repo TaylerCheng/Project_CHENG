ece4350061fac2d780889f4204089fff0b34a37b
Tayler Cheng <guangchengcc@163.com>
2017-01-20 13:55:36 +0800

    完善Yarn集群Job提交工具

--- HadoopStudy/src/main/java/com/cg/mapreduce/wordcount/WordCount.java
+++ HadoopStudy/src/main/java/com/cg/mapreduce/wordcount/WordCount.java
@@ -1,73 +1,66 @@
 package com.cg.mapreduce.wordcount;
 
-import java.io.File;
-import java.io.IOException;
-
 import com.cg.utils.YarnJobUtil;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-import com.cg.utils.EJob;
 import org.apache.log4j.Logger;
 
 public class WordCount {
 
     public static Logger logger = Logger.getLogger(WordCount.class);
 
+    public static final String JOB_NAME = "WordCount";
+    /**
+     *  LOCAL为本地执行，CLUSTER则提交到YARN集群上执行
+     */
+    public static final ExecuteMode executeMode = ExecuteMode.CLUSTER;
+
     public static void main(String[] args) throws Exception {
+
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args)
                 .getRemainingArgs();
-        if (otherArgs.length != 2) {
-            System.err.println("parameters should >= 2");
+        if (otherArgs.length < 3) {
+            logger.error("parameters should >= 3");
             System.exit(2);
         }
 
-        //LOCAL为本地执行，CLUSTER则提交到YARN集群上执行
-        ExecuteMode executeMode = ExecuteMode.LOCAL;
-
-        // Section 1
+        // Section 1 init job
         Job job = null;
         if (executeMode.equals(ExecuteMode.CLUSTER)) {
-            String outPutPath = "E:\\gitspace\\Project_CHENG\\HadoopStudy\\target\\classes";
+            String classpath = otherArgs[1];
             conf.set("mapred.reduce.tasks", "2");
-            job = YarnJobUtil.initJob(conf, outPutPath);
-            if (job == null) {
-                logger.error("yarn-mode init failed!");
-                System.exit(0);
-            }
+            job = YarnJobUtil.initJob(conf, JOB_NAME, classpath);
         } else {
-            job = new Job(conf, "wordcount");
+            job = Job.getInstance(conf, JOB_NAME);
             job.setJarByClass(WordCount.class);
         }
 
-        // Section 2
+        // Section 2 job config
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
-
-        // Section 3
         job.setInputFormatClass(TextInputFormat.class);
-        TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        Path outputPath = new Path(otherArgs[1]);
+        TextInputFormat.addInputPath(job, new Path(otherArgs[1]));
+        Path outputPath = new Path(otherArgs[2]);
         FileSystem fs = outputPath.getFileSystem(conf);
         if (fs.exists(outputPath)) {
-            fs.delete(outputPath);
+            fs.delete(outputPath,true);
         }
         job.setOutputFormatClass(TextOutputFormat.class);
         TextOutputFormat.setOutputPath(job, outputPath);
 
-        // Section 4
+        // Section 3 excute job
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
