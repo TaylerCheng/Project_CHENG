f8b43477ed98ee73fd75ab94e8930e693a3408bc
Tayler Cheng <guangchengcc@163.com>
2017-03-22 10:56:19 +0800

    添加新的main函数

---
+++ dev-main-v-2.0/src/main/java/com/niuwa/hadoop/chubao/ChuBaoMain.java
@@ -0,0 +1,125 @@
+package com.niuwa.hadoop.chubao;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
+import com.niuwa.hadoop.chubao.Main;
+import com.niuwa.hadoop.chubao.job.*;
+import com.niuwa.hadoop.chubao.utils.ChubaoDateUtil;
+import com.niuwa.hadoop.util.HadoopUtil;
+import org.apache.commons.cli.Options;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 触宝白名单任务启动类
+ * 
+ * @author Administrator
+ *
+ */
+public class ChuBaoMain {
+
+	public static int[] rule = new int[10];
+
+	private static final Logger log= LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) throws Exception {
+//		HadoopUtil.isWinOrLiux();
+
+		long startTime = System.currentTimeMillis();
+		runJobs(args);
+		long endTime = System.currentTimeMillis();
+
+		log.info("[共耗时]:{}(s)", (endTime - startTime) / 1000);
+		log.info("[isDebugMode]{}", ChubaoJobConfig.isDebugMode());
+		log.info("[dataLatestTime]{}",
+				new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(ChubaoDateUtil.dataLastedTime.getTime()));
+		log.info("[job params]{}", JSONObject.toJSONString(args));
+
+		if (ChubaoJobConfig.isDebugMode()) {
+			log.info("white list passed:[{}]", rule[0]);
+			for (int i = 1; i < rule.length; i++) {
+				log.info("rule-" + i + " passed:[{}]", rule[i]);
+			}
+		}
+
+	}
+
+	private static void runJobs(String[] args) throws Exception {
+		//生成命令行解析参数
+		Options options = ChubaoCommandLine.buildOptions();
+		//解析输入的命令行参数
+		RunParams params = new RunParams();
+		boolean isSuc = ChubaoCommandLine.parse(args, options, params);
+		if(!isSuc){
+			return;
+		}
+
+		//初始化触宝全局配置
+		ChubaoJobConfig.initChubaoConfig(params);
+
+		//初始化job运行配置
+		Configuration conf = new Configuration();
+		ChubaoJobConfig.initConfiguration(params, conf);
+
+		//获取所有需要运行的job
+		List<String> allJobsTobeRun = getAllJobsToBeRun(params);
+
+		//将中间结果文件目录放到一个map中方便查找
+		Map<String, Path> tempPaths= ChubaoJobConfig.genPathsToStoreTempResults(allJobsTobeRun, params.isSmall());
+
+		//初始化control job
+		Map<BaseJob, ControlledJob> controlledJobMap = new HashMap<>();
+		ChubaoJobConfig.initControlledJobMap(params, conf, allJobsTobeRun, tempPaths, controlledJobMap);
+
+
+		// 创建JobControl
+		String jobControlName = params.isSmall()?"Small":"Large"+" White List JobGroup";
+		JobControl jobControl = new JobControl(jobControlName);
+		for(BaseJob baseJob : controlledJobMap.keySet()){
+			jobControl.addJob(controlledJobMap.get(baseJob));
+		}
+
+		// 启动任务
+		Thread jobControlThread = new Thread(jobControl);
+		jobControlThread.start();
+		while (true) {
+			if (jobControl.allFinished()) {
+				List<ControlledJob> successfulJobList = jobControl.getSuccessfulJobList();
+				System.out.println(jobControl.getSuccessfulJobList());
+				jobControl.stop();
+				break;
+			}
+		}
+	}
+
+	//此处需要根据每次实际运行的job进行修改，main函数中的整个调用流程不用修改
+	//每个元素为对应job的类名
+	private static List<String> getAllJobsToBeRun(RunParams params){
+		List<String> allJobsTobeRun = null;
+		
+		if(params.isSmall()) {
+//			allJobsTobeRun = Lists.newArrayList(CallLogJob001.class.getName(), CallLogJob002.class.getName(),
+//					IndicatorJob002.class.getName(), IndicatorJob005.class.getName(), NewIndicatorJob006.class.getName(),
+//					IndicatorJob007.class.getName(), LargeIndicatorJob006.class.getName(), NewJugementJob.class.getName());
+			allJobsTobeRun = Lists.newArrayList(CallLogJob001.class.getName());
+		}else{
+			allJobsTobeRun = Lists.newArrayList(LargeIndicatorJob001.class.getName(), LargeIndicatorJob002.class.getName(),
+					LargeIndicatorJob003.class.getName(), LargeIndicatorJob004.class.getName(), LargeIndicatorJob005.class.getName(),
+					LargeIndicatorJob006.class.getName(),
+					LargeJugementJob.class.getName());
+		}
+		
+		return allJobsTobeRun;
+	}
+	
+
+}
---
+++ dev-main-v-2.0/src/main/java/com/niuwa/hadoop/chubao/job/NewJugementJob.java
@@ -0,0 +1,275 @@
+package com.niuwa.hadoop.chubao.job;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Sets;
+import com.niuwa.hadoop.chubao.*;
+import com.niuwa.hadoop.chubao.rules.Rules;
+import com.niuwa.hadoop.util.HadoopUtil;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Map;
+
+/**
+ * 
+ * 小额指标合并过滤任务<br> 
+ * 联合过滤规则和判断条件，输出最终白名单结果：
+ * 输出结果：
+ * user_id：用户唯一标识
+ * type:1:小额，2:大额
+ * amount:最终可借款金额
+ * prod_rate:产品费率
+ * daily_fee_rate:日费率
+ * month_fee_rate:月费率（小额默认为0）
+ * base_fee_rate:基本费率
+ * @author maliqiang
+ * @see [相关类/方法]（可选）
+ * @since 2016-7-4
+ */
+public class NewJugementJob extends BaseJob {
+
+    public static class MapSevralTemp extends NiuwaMapper<Object, Text, Text, Text>{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+
+			JSONObject record= JSONObject.parseObject(value.toString());
+
+			context.write(new Text(record.getString("user_id")), value);
+		}
+	}
+
+	public static class ReduceObj extends NiuwaReducer<Text, Text, NullWritable, Text>{
+		public static final Logger log = LoggerFactory.getLogger(ReduceObj.class);
+
+		/**
+		 *  触宝费率基础
+		 *  {
+		 *		"base_prod_rate": 0.108,
+		 *		"base_daily_fee_rate": 0.005,
+		 *		"base_month_fee_rate": 0.00,
+		 *		"base_fee_rate": 0.01
+		 *	}
+		 */
+		private JSONObject baseRateRecord= null;
+
+		// 最终结果
+ 		private JSONObject finalObj= new JSONObject();
+
+		public void setup(Context context){
+	        /**
+	         * 读取cachefiles
+	         *
+	         * 官方文档使用这个方法，测试使用上下文也能获取到，还不知道问题所在
+	         * URI[] patternsURIs = Job.getInstance(context.getConfiguration()).getCacheFiles();
+	         */
+	        super.setup(context);
+			BufferedReader reader=null;
+	        try{
+
+		        URI[] paths =context.getCacheFiles();
+		        log.info("[ cached file number ]{}"+ paths.length);
+		        //Path cacheFilePath= new Path(paths[1].getPath());
+		        Path cacheFilePath= new Path(paths[0].getPath());
+		        reader= new BufferedReader(new FileReader(cacheFilePath.getName().toString()));
+		        StringBuffer content= new StringBuffer();
+		        String str= null;
+	            while((str= reader.readLine())!=null){
+	            	content.append(str);
+	            }
+	            baseRateRecord= JSONObject.parseObject(content.toString());
+	        }catch(Exception e){
+	            e.printStackTrace();
+	        }finally{
+	            try {
+					reader.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+		}
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+			JSONObject resultObj= new JSONObject();
+			resultObj.put("user_id", key.toString());
+            DecimalFormat df = new DecimalFormat("#.00000");
+            DecimalFormat df2 = new DecimalFormat("#.00");
+
+            initResultObj(resultObj);
+
+			for(Text value : values){
+				JSONObject lineObj= JSONObject.parseObject(value.toString());
+
+				for(Map.Entry<String, Object> entry: lineObj.entrySet()) {
+					resultObj.put(entry.getKey(), entry.getValue());
+				}
+			}
+
+			boolean rule = JudgeRules(resultObj);
+            //DEBUG输出符合规则和不符合规则的；非DEBUG，只输出符合规则的
+            if(ChubaoJobConfig.isDebugMode() || rule){
+				finalObj.put("user_id", key.toString());
+				finalObj.put("type", 1);
+				//可借款最终金额
+				finalObj.put("amount", Double.parseDouble(df2.format(resultObj.getDouble("final_amount"))));
+				finalObj.put("prod_rate",
+						Double.parseDouble(df.format(baseRateRecord.getDouble("base_prod_rate"))));//APR
+				finalObj.put("daily_fee_rate",
+						Double.parseDouble(df.format(baseRateRecord.getDouble("daily_fee_rate"))));// 日手续费
+				finalObj.put("monthly_fee_rate",
+						Double.parseDouble(df.format(baseRateRecord.getDouble("base_month_fee_rate") + 0)));// 月手续费
+				finalObj.put("base_fee_rate",
+						Double.parseDouble(df.format(resultObj.getDouble("base_fee_rate"))));// 基本手续费
+
+                /*输出结果：
+                user_id：用户唯一标识
+                type:1:小额，2:大额
+                amount:最终可借款金额
+                prod_rate:产品费率
+                daily_fee_rate:日费率
+                month_fee_rate:月费率（小额默认为0）
+                base_fee_rate:基本费率
+                */
+				context.write(NullWritable.get(), new Text(finalObj.toJSONString()));
+            }
+
+		}
+
+		private boolean JudgeRules(JSONObject resultObj) {
+			boolean rule1 = Rules.isMatchedRule_1(resultObj.getLong("device_activation"));
+			boolean rule2 = Rules.isMatchedRule_2(resultObj.getInteger("call_num_3_month"));
+			boolean rule3 = Rules.rule3(resultObj);
+//			boolean rule4 = Rules.isMatchedRule4(resultObj.getInteger("total_calls_from_tel_library"),
+//					resultObj.getInteger("total_diff_num_called_from_tel_library"));
+			boolean rule5 = Rules.rule5(resultObj);
+			boolean rule6 = Rules.rule6(resultObj.getIntValue("total_apps_from_app_library"));
+			boolean rule7 = Rules.rule7(resultObj.getDoubleValue("break_ratio"));
+			boolean rule8 = Rules.rule8(resultObj.getDoubleValue("good_cnt_rate"),resultObj.getDoubleValue("bad_cnt_rate"));
+            boolean rule9 = Rules.rule9(resultObj.getIntValue("user_loan_overdue"));
+
+            //规则4下线  2017/02/24
+//          boolean rule = rule1 && rule2 && rule3 && rule4 && rule5 && rule6 && rule7 && rule8 && rule9;
+			boolean rule = rule1 && rule2 && rule3 && rule5 && rule6 && rule7 && rule8 && rule9;
+
+			//DEBUG模式输出规则是否命中
+			if(ChubaoJobConfig.isDebugMode()){
+				if (rule){
+					ChuBaoMain.rule[0]++;
+				}
+				if (rule1) {
+					ChuBaoMain.rule[1]++;
+				}
+				if (rule2) {
+					ChuBaoMain.rule[2]++;
+				}
+				if (rule3) {
+					ChuBaoMain.rule[3]++;
+				}
+				if (rule5) {
+					ChuBaoMain.rule[5]++;
+				}
+				if (rule6) {
+					ChuBaoMain.rule[6]++;
+				}
+				if (rule7) {
+					ChuBaoMain.rule[7]++;
+				}
+				if (rule8) {
+					ChuBaoMain.rule[8]++;
+				}
+				if (rule9) {
+					ChuBaoMain.rule[9]++;
+				}
+			    finalObj.put("rule1", rule1);
+			    finalObj.put("rule2", rule2);
+			    finalObj.put("rule3", resultObj.get("rule_type"));
+//			    finalObj.put("rule4", rule4);
+			    finalObj.put("rule5", rule5);
+			    finalObj.put("rule6", rule6);
+			    finalObj.put("rule7", rule7);
+                finalObj.put("rule8", rule8);
+                finalObj.put("rule9", rule9);
+
+				finalObj.put("pass", rule);
+            }
+			return rule;
+		}
+
+		private void initResultObj(JSONObject resultObj) {
+			//rule1默认值
+			//激活时间默认成最大值，这样激活时间判断，不符合的肯定不过，符合的会将该值重写
+            resultObj.put("device_activation", Long.MAX_VALUE);
+
+			//rule2默认值
+            resultObj.put("call_num_3_month", 0);
+
+            //rule3默认值
+            resultObj.put("contact_sum", 0);
+            resultObj.put("max_contact_call", 0);
+            resultObj.put("call_true_rate_type", 0.0);
+
+            //rule4默认值
+			//没有通话记录时，来自号码库号码拨打次数默认为0，来自不同号码数目默认为0
+//            resultObj.put("total_calls_from_tel_library", 0);
+//            resultObj.put("total_diff_num_called_from_tel_library", 0);
+
+            //rule5默认值
+            resultObj.put("m2_records", 0);
+            resultObj.put("loan_max_out_day", 0);
+
+            //rule6默认值
+            resultObj.put("total_apps_from_app_library", 0);
+            //rule7默认值
+            resultObj.put("break_ratio", 0.0);
+		}
+	}
+
+	@Override
+	public void setJobSpecialInfo(Job job, Configuration conf,
+			RunParams params,
+			Map<String, Path> tempPaths) throws Exception{
+
+
+		job.setMapperClass(NewJugementJob.MapSevralTemp.class);
+		job.setReducerClass(NewJugementJob.ReduceObj.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+	
+		// 读取静态缓存的费率配置文件
+		job.addCacheFile(ChubaoJobConfig.getConfigPath("rate-config.txt").toUri());
+        // 输入路径
+		FileInputFormat.addInputPath(job, tempPaths.get(CallLogJob002.class.getName()));
+		FileInputFormat.addInputPath(job, tempPaths.get(IndicatorJob002.class.getName()));
+		FileInputFormat.addInputPath(job, tempPaths.get(NewIndicatorJob006.class.getName()));
+		FileInputFormat.addInputPath(job, tempPaths.get(IndicatorJob007.class.getName()));
+		FileInputFormat.addInputPath(job, tempPaths.get(LargeIndicatorJob006.class.getName()));
+
+		// 输出路径
+		Path outputFile= ChubaoJobConfig.getOutputPath("small");
+		if(ChubaoJobConfig.isDebugMode()){
+			outputFile= ChubaoJobConfig.getOutputPath("small-debug");
+		}
+		FileOutputFormat.setOutputPath(job, outputFile);
+		// 删除原有的输出
+		HadoopUtil.deleteOutputFile(outputFile);
+		
+	}
+
+	@Override
+	public final HashSet<String> getDependingJobNames() {
+		return Sets.newHashSet(CallLogJob002.class.getName(), IndicatorJob002.class.getName(),
+				IndicatorJob006.class.getName(), IndicatorJob007.class.getName(), LargeIndicatorJob006.class.getName());
+	}
+}
