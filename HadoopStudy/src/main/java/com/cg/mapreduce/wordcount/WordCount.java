8cc149d0f884e6de9fcba28938987178ac206120
Tayler Cheng <guangchengcc@163.com>
2017-03-23 09:39:00 +0800

    改用ToolRunner

--- HadoopStudy/src/main/java/com/cg/mapreduce/wordcount/WordCount.java
+++ HadoopStudy/src/main/java/com/cg/mapreduce/wordcount/WordCount.java
@@ -2,6 +2,7 @@ package com.cg.mapreduce.wordcount;
 
 import com.cg.mapreduce.utils.YarnJobUtil;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -13,11 +14,13 @@ import org.apache.hadoop.mapreduce.JobCounter;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
 import java.io.File;
 
-public class WordCount {
+public class WordCount extends Configured implements Tool {
 
     public static Logger logger = Logger.getLogger(WordCount.class);
 
@@ -25,27 +28,26 @@ public class WordCount {
     // LOCAL为本地执行，CLUSTER则提交到YARN集群上执行
     public static final ExecuteMode executeMode = ExecuteMode.LOCAL;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         long startTime = System.currentTimeMillis();
-        boolean completed = runJob(args);
+        int success = ToolRunner.run(new WordCount(), args);
         long endTime = System.currentTimeMillis();
         System.out.println("耗时：" + (endTime - startTime) / 1000);
-        System.exit(completed ? 0 : 1);
+        System.exit(success);
     }
 
-    private static boolean runJob(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        String[] otherArgs = new GenericOptionsParser(conf, args)
-                .getRemainingArgs();
+    @Override
+    public int run(String[] otherArgs) throws Exception {
         if (otherArgs.length < 2) {
             logger.error("parameters should >= 2");
             System.exit(2);
         }
 
         // Section 1 init job
+        Configuration conf = getConf();
         Job job = null;
         if (executeMode.equals(ExecuteMode.CLUSTER)) {
-            conf.set("mapreduce.job.reduces", "2");
+            conf.set("mapreduce.job.reduces", "3");
             job = Job.getInstance(conf, JOB_NAME);
             String classpath = otherArgs[2];
             File jarfile = YarnJobUtil.getJobJarFile(classpath);
@@ -73,7 +75,7 @@ public class WordCount {
         Path outputPath = new Path(otherArgs[1]);
         FileSystem fs = outputPath.getFileSystem(conf);
         if (fs.exists(outputPath)) {
-            fs.delete(outputPath,true);
+            fs.delete(outputPath, true);
         }
         job.setOutputFormatClass(TextOutputFormat.class);
         job.setOutputKeyClass(Text.class);
@@ -81,15 +83,7 @@ public class WordCount {
         TextOutputFormat.setOutputPath(job, outputPath);
 
         // Section 3 excute job
-        boolean completed = job.waitForCompletion(true);
-
-        Counters counters = job.getCounters();
-        Counter counter1 = counters.findCounter(JobCounter.MILLIS_MAPS);
-        System.out.println(counter1.getValue());
-        Counter counter2 = counters.findCounter(JobCounter.MILLIS_REDUCES);
-        System.out.println(counter2.getValue());
-        System.out.println((job.getFinishTime() - job.getStartTime()) / 1000);
-        return completed;
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 
 }
