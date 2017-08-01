87d04e64bbc0d9c3f69a09d77498d91075911fcd
Tayler Cheng <guangchengcc@163.com>
2017-08-01 10:11:49 +0800

    Scala学习

--- ScalaStudy/src/com/cg/scala/section_6/Section_6.scala
+++ ScalaStudy/src/com/cg/scala/section_6/Section_6.scala
@@ -1,9 +1,10 @@
 package com.cg.scala.section_6
 
 /**
+  * 第六节 类定义、创建对象
   *
   * @author ： Cheng Guang
-  **/
+  */
 object Section_6 {
   var studentNo:Int=0;
 
@@ -19,6 +20,11 @@ object Section_6 {
     p.getName
     p.age_=(5)
     print(p)
+  }
+
+  def test(i: Int) = {
+    studentNo += 1
+    studentNo
   }
 
 }
